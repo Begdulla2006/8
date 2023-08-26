af0764eb1185e59a6d0083ae5612041a26f7174a
unknown <BEGDULLA2006.com>
2023-08-26 15:32:54 +0500

    Second

--- src/js/set.js
+++ src/js/set.js
@@ -39,7 +39,7 @@ const polswiper = new Swiper('.pol-swiper', {
 // $ git config --global user.email johndoe@example.com
 //   git commit -m "First"
 //   git branch -M main
-//   git remote add origin https://github.com/Begdulla2006/7.git
+//   git remote add origin https://github.com/Begdulla2006/8.git
 //   git push -u origin main
 
 //   git add .
